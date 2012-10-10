341165d6d0bb447964f939321296d89a7f9a6d09
James Tunnell <jamestunnell@lavabit.com>
2012-10-09 17:51:49 -0700

    fix Pitch#ratio= and add support for :ratio and :total_cent hash args in Pitch#initialize. Add tests to go along.

--- lib/musicality/core/pitch.rb
+++ lib/musicality/core/pitch.rb
@@ -3,60 +3,62 @@ module Musicality
   class Pitch
 	include Comparable
   
-	attr_reader :octave, :semitone, :cent
+	attr_reader :octave, :semitone, :cent, :semitones_per_octave, :cents_per_octave
 
 	SEMITONES_PER_OCTAVE = 12
 	CENTS_PER_SEMITONE = 100
 
 	def initialize args={}
-	  if args[:octave]
-		raise ArgumentError, "args[:octave] #{args[:octave]} is not a Fixnum" if !args[:octave].is_a?(Fixnum)
+      @cents_per_octave = CENTS_PER_SEMITONE * SEMITONES_PER_OCTAVE
+      
+	  if args[:total_cent]
+	    self.total_cent = args[:total_cent] 
+	  elsif args[:ratio]
+	    self.ratio = args[:ratio]
+	  else
+	    if args[:octave]
+		  raise ArgumentError, "args[:octave] #{args[:octave]} is not a Fixnum" if !args[:octave].is_a?(Fixnum)
+	    end
+	    @octave = args[:octave] || 0
+	  
+	    if args[:semitone]
+		  raise ArgumentError, "args[:semitone] #{args[:semitone]} is not a Fixnum" if !args[:semitone].is_a?(Fixnum)
+	    end
+	    @semitone = args[:semitone] || 0
+	  
+	    if args[:cent]
+		  raise ArgumentError, "args[:cent] #{args[:cent]} is not a Fixnum" if !args[:cent].is_a?(Fixnum)
+	    end
+	    @cent = args[:cent] || 0      
+			
+	    normalize
 	  end
-	  @octave = args[:octave] || 0
 	  
-	  if args[:semitone]
-		raise ArgumentError, "args[:semitone] #{args[:semitone]} is not a Fixnum" if !args[:semitone].is_a?(Fixnum)
-	  end
-	  @semitone = args[:semitone] || 0
-	  
-	  if args[:cent]
-		raise ArgumentError, "args[:cent] #{args[:cent]} is not a Fixnum" if !args[:cent].is_a?(Fixnum)
-	  end
-	  @cent = args[:cent] || 0
-	  
-	  @cents_per_octave = CENTS_PER_SEMITONE * SEMITONES_PER_OCTAVE
-      normalize
-	  
-	  #if args[:ratio]
-	  #  ratio = args[:ratio]
-	  #end
 	end
-    
+
 	def total_cent
 	  return (@octave * @cents_per_octave) +
 						(@semitone * CENTS_PER_SEMITONE) + @cent
 	end
-    
+	
 	def total_cent= cent
 	  @octave, @semitone, @cent = 0, 0, cent
 	  normalize
 	end
-	
+
 	def ratio
-	  2.0**(total_cent.to_f / @cents_per_octave)
-	end
-	
-	def ratio= new_ratio
-	  raise ArgumentError, "ratio #{new_ratio} is not a Float" if !new_ratio.is_a?(Float)
-	  raise RangeError, "ratio #{new_ratio} is less than or equal to 0.0" if new_ratio <= 0.0
-		  
-	  x = Math.log2 new_ratio
-		
-	  total_cent = (x * @cents_per_octave).round
+	  2.0**(self.total_cent.to_f / @cents_per_octave)
 	end
 
+	def ratio= ratio
+	  raise RangeError, "ratio #{ratio} is less than or equal to 0.0" if ratio <= 0.0
+	  
+	  x = Math.log2 ratio
+	  self.total_cent = (x * @cents_per_octave).round
+	end
+	
 	def <=> (other)
-	  total_cent <=> other.total_cent
+	  self.total_cent <=> other.total_cent
 	end
 	
 	def + (other)
@@ -70,7 +72,7 @@ module Musicality
 	private
 		
 	def normalize
-	  centTotal = (octave * @cents_per_octave) + (semitone * CENTS_PER_SEMITONE) + cent
+	  centTotal = (@octave * @cents_per_octave) + (@semitone * CENTS_PER_SEMITONE) + @cent
 	  
 	  @octave = centTotal / @cents_per_octave
 	  centTotal -= @octave * @cents_per_octave
--- spec/core/pitch_spec.rb
+++ spec/core/pitch_spec.rb
@@ -19,7 +19,24 @@ describe Musicality::Pitch do
       actual > expected
     end
   end
-    
+  
+  before :each do
+    @cases = 
+    [
+      { :octave => 1, :semitone => 0, :cent => 0, :ratio => 2.0, :total_cent => 1200 },
+      { :octave => 2, :semitone => 0, :cent => 0, :ratio => 4.0, :total_cent => 2400 },
+      { :octave => 1, :semitone => 6, :cent => 0, :ratio => 2.8284, :total_cent => 1800 },
+      { :octave => 2, :semitone => 6, :cent => 0, :ratio => 5.6569, :total_cent => 3000 },
+      { :octave => 1, :semitone => 6, :cent => 20, :ratio => 2.8613, :total_cent => 1820 },
+      { :octave => 2, :semitone => 6, :cent => 20, :ratio => 5.7226, :total_cent => 3020 },
+      { :octave => 3, :semitone => 7, :cent => 77, :ratio => 12.5316, :total_cent => 4377 },
+      { :octave => -1, :semitone => 0, :cent => 0, :ratio => 0.5, :total_cent => -1200 },
+      { :octave => -2, :semitone => 0, :cent => 0, :ratio => 0.25, :total_cent => -2400 },
+      { :octave => -2, :semitone => 7, :cent => 55, :ratio => 0.3867, :total_cent => -1645 },
+      { :octave => -1, :semitone => 9, :cent => 23, :ratio => 0.8521, :total_cent => -277 },
+    ]
+  end
+  
   it "should be constructible with no parameters (no ArgumentError raised)" do
 	  lambda { Musicality::Pitch.new }.should_not raise_error ArgumentError
   end
@@ -30,26 +47,33 @@ describe Musicality::Pitch do
     p.total_cent.should eq(0)
   end
   
-  it "should use the octave, semitone, and cent given" do
-    cases = 
-    { 
-      [1,0,0] => { :ratio => 2.0, :total_cent => 1200 },
-      [2,0,0] => { :ratio => 4.0, :total_cent => 2400 },
-      [1,6,0] => { :ratio => 2.8284, :total_cent => 1800 },
-      [2,6,0] => { :ratio => 5.6569, :total_cent => 3000 },
-      [1,6,20] => { :ratio => 2.8613, :total_cent => 1820 },
-      [2,6,20] => { :ratio => 5.7226, :total_cent => 3020 },
-      [3,7,77] => { :ratio => 12.5316, :total_cent => 4377 },
-      [-1,0,0] => { :ratio => 0.5, :total_cent => -1200 },
-      [-2,0,0] => { :ratio => 0.25, :total_cent => -2400 },
-      [-2,7,55] => { :ratio => 0.3867, :total_cent => -1645 },
-      [1,-14,-77] => { :ratio => 0.8521, :total_cent => -277 },
-    }
-    
-    cases.each do |input, expected|
-      p = Musicality::Pitch.new :octave => input[0], :semitone => input[1], :cent => input[2]
-      p.ratio.should be_almost(expected[:ratio])
-      p.total_cent.should be expected[:total_cent]
+  it "should use the octave, semitone, and cent given during construction" do
+    @cases.each do |case_data|
+      p = Musicality::Pitch.new :octave => case_data[:octave], :semitone => case_data[:semitone], :cent => case_data[:cent]
+      p.ratio.should be_almost case_data[:ratio]
+      p.total_cent.should be case_data[:total_cent]
+    end
+  end
+
+  it "should use the ratio given during construction" do
+    @cases.each do |case_data|
+      p = Musicality::Pitch.new :ratio => case_data[:ratio]
+            
+      p.octave.should eq case_data[:octave]
+      p.semitone.should eq case_data[:semitone]
+      p.cent.should eq case_data[:cent]
+      p.total_cent.should eq case_data[:total_cent]
+    end
+  end
+
+  it "should use the total_cent given during construction" do    
+    @cases.each do |case_data|
+      p = Musicality::Pitch.new :total_cent => case_data[:total_cent] 
+      
+      p.octave.should eq case_data[:octave]
+      p.semitone.should eq case_data[:semitone]
+      p.cent.should eq case_data[:cent]
+      p.total_cent.should eq case_data[:total_cent]
     end
   end
   
