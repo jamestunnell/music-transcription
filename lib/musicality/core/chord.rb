3bd1eaf8fbc6eb73b430d3b0bcd72b8345e92655
James Tunnell <jamestunnell@lavabit.com>
2012-10-09 22:00:34 -0700

    Handle :arpeggiate arg in Chord#initialize hash args, which indicates that a chord is to be arpeggiated when played. Verify that the :notes arg is an Array, and each element is a Note, and that each Note is the same length.

--- lib/musicality/core/chord.rb
+++ lib/musicality/core/chord.rb
@@ -1,10 +1,26 @@
 module Musicality
 
 class Chord
-  attr_reader :notes
+  attr_reader :notes, :arpeggiate
   
   def initialize args={}
+    
+    if args[:notes]
+      raise ArgumentError, "args[:notes] is not an Array" if !args[:notes].is_a?(Array)
+      
+      if !args[:notes].empty?
+        for i in 0...args[:notes].length do
+          raise ArgumentError, "note #{args[:notes][i]} in args[:notes] is not a note" if !args[:notes][i].is_a?(Note)
+          
+          if i > 0
+            raise ArgumentError, "length of note #{i} in #{args[:notes]} is not the same as length of first note." if args[:notes][i].duration != args[:notes][0].duration
+          end
+        end
+      end      
+    end
+    
     @notes = args[:notes] || []
+    @arpeggiate = args[:arpeggiate] || false
   end
 end
 
--- spec/core/chord_spec.rb
+++ spec/core/chord_spec.rb
@@ -13,12 +13,52 @@ describe Musicality::Chord do
   it "should assign the :notes given during construction" do
     notes = 
     [
-      Musicality::Note.new({:ratio => 1.5}),
-      Musicality::Note.new({:ratio => 2.5}),
-      Musicality::Note.new({:ratio => 25.0})
+      Musicality::Note.new(:pitch => Musicality::Pitch.new({:ratio => 1.5})),
+      Musicality::Note.new(:pitch => Musicality::Pitch.new({:ratio => 1.5})),
+      Musicality::Note.new(:pitch => Musicality::Pitch.new({:ratio => 1.5})),
     ]
     chord = Musicality::Chord.new :notes => notes
-    chord.notes.should eq(notes)
+    chord.notes.should eq(notes.clone)
   end
 
+  it "should not raise ArgumentError if notes are the same length" do
+    notes = 
+    [
+      Musicality::Note.new({:duration => 0.25.to_r}),
+      Musicality::Note.new({:duration => 0.25.to_r}),
+      Musicality::Note.new({:duration => 0.25.to_r})
+    ]
+    lambda { Musicality::Chord.new :notes => notes }.should_not raise_error ArgumentError
+  end
+
+  it "should raise ArgumentError if a non-Array is given for :notes" do
+    lambda { Musicality::Chord.new :notes => { "a" => "b"} }.should raise_error ArgumentError
+  end
+
+  it "should raise ArgumentError if a non-Note is part of the Array given for :notes" do
+    notes = [ Musicality::Note.new, Musicality::Note.new, 5 ]
+    lambda { Musicality::Chord.new :notes => notes }.should raise_error ArgumentError
+  end
+    
+  it "should raise ArgumentError if notes are not the same length" do
+    notes = 
+    [
+      Musicality::Note.new({:duration => 1.5.to_r}),
+      Musicality::Note.new({:duration => 1.5.to_r}),
+      Musicality::Note.new({:duration => 1.25.to_r})
+    ]
+    
+    lambda { Musicality::Chord.new :notes => notes }.should raise_error ArgumentError
+  end
+  it "should set arpeggiate to false if not given during construction" do
+    chord = Musicality::Chord.new
+    chord.arpeggiate.should be false
+  end
+  
+  it "should assign the :arpeggiate flag as given during construction" do
+    chord = Musicality::Chord.new :arpeggiate => true
+    chord.arpeggiate.should be true
+    chord = Musicality::Chord.new :arpeggiate => false
+    chord.arpeggiate.should be false
+  end
 end
