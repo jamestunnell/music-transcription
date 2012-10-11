df2abab40cc3aa8504fe529b4f163842cf50b716
James Tunnell <jamestunnell@lavabit.com>
2012-10-10 21:49:19 -0700

    For Note class:  -add accessors for all instance variables  -move range and type checking out of constructor and into accessors  -call accessors from constructor  -add tests for accessors

--- lib/musicality/core/note.rb
+++ lib/musicality/core/note.rb
@@ -5,50 +5,114 @@ module Musicality
 #
 # @author James Tunnell
 # 
-# @!attribute [r] pitch
+# @!attribute [rw] pitch
 #   @return [Pitch] The pitch of the note.
-# @!attribute [r] duration
-#   @return [Rational] the duration of the note, in note lengths (e.g. whole note => 1/1, quarter note => 1/4).
-# @!attribute [r] intensity
-#   @return [Float] Affects the loudness (envelope) during the attack portion of the note. From 0.0 (less attack) to 1.0 (more attack).
-# @!attribute [r] loudness
-#   @return [Float] Affects the loudness (envelope) during the sustain portion of the note. From 0.0 (less sustain) to 1.0 (more sustain).
-# @!attribute [r] seperation
-#   @return [Float] Shift the note release towards or away the beginning of the note. From 0.0 (towards end of the note) to 1.0 (towards beginning of the note).
+#
+# @!attribute [rw] duration
+#   @return [Rational] the duration of the note, in note lengths (e.g. 
+#                      whole note => 1/1, quarter note => 1/4).
+#
+# @!attribute [rw] intensity
+#   @return [Float] Affects the loudness (envelope) during the attack 
+#                   portion of the note. From 0.0 (less attack) to 1.0 
+#                   (more attack).
+#
+# @!attribute [rw] loudness
+#   @return [Float] Affects the loudness (envelope) during the sustain 
+#                   portion of the note. From 0.0 (less sustain) to 1.0 
+#                   (more sustain).
+#
+# @!attribute [rw] seperation
+#   @return [Float] Shift the note release towards or away the beginning
+#                   of the note. From 0.0 (towards end of the note) to 
+#                   1.0 (towards beginning of the note).
+#
+# @!attribute [rw] tie
+#   @return [true/false] Indicates the note should be played 
+#                        continuously with the following note of the 
+#                        same pitch (if such a note exists).
+#
+# @!attribute [rw] slur
+#   @return [true/false] Indicates the following note (if such a note 
+#                        exists) should be played without rearticulation
+#                        (i.e. no attack).
+#
 class Note
 
   attr_reader :pitch, :duration, :loudness, :intensity, :seperation
+  attr_accessor :tie, :slur
   
   # A new instance of Note.
   # @param [Pitch] pitch The pitch of the note.
-  # @param [Rational] duration The duration of the note, in note lengths (e.g. whole note => 1/1, quarter note => 1/4).
-  # @param [Hash] options Optional arguments. Valid keys are :loudness, :intensity, and :seperation 
+  # @param [Rational] duration The duration of the note, in note lengths
+  #                   (e.g. whole note => 1/1, quarter note => 1/4).
+  # @param [Hash] options Optional arguments. Valid keys are :loudness,
+  #               :intensity, :seperation, :tie, and :slur.
   def initialize pitch, duration, options={}
-    raise ArgumentError, "pitch is not a Pitch" if !pitch.is_a?(Pitch)
-    raise ArgumentError, "duration is not a Rational" if !duration.is_a?(Rational)    
-
-	  @pitch = pitch
-	  @duration = duration
-	
-	  opts = {
-	    :loudness => 0.5,
-	    :intensity => 0.5,
-	    :seperation => 0.5
-	  }.merge options
+    self.pitch = pitch
+    self.duration = duration
+  
+    opts = {
+      :loudness => 0.5,
+      :intensity => 0.5,
+      :seperation => 0.5,
+      :tie => false,
+      :slur => false,
+    }.merge options
 	  
-	  # The loudness, intensity, and seperation will be used to form the envelope profile for the note.
+    # The loudness, intensity, and seperation will be used to form the envelope profile for the note.
 
-    @loudness = opts[:loudness]
-    raise ArgumentError, "options[:loudness] is not a Float" if !@loudness.is_a?(Float)
-    raise RangeError, "options[:loudness] is outside the range 0.0..1.0" if !(0.0..1.0).include?(@loudness)
+    self.loudness = opts[:loudness]
+    self.intensity = opts[:intensity]	
+	self.seperation = opts[:seperation]
+    self.tie = opts[:tie]
+    self.slur = opts[:slur]
+  end
+  
+  # Set the note pitch.
+  # @param [Pitch] pitch The pitch of the note.
+  # @raise [ArgumentError] if pitch is not a Pitch.
+  def pitch= pitch
+    raise ArgumentError, "pitch is not a Pitch" if !pitch.is_a?(Pitch)
+    @pitch = pitch
+  end
 
-    @intensity = opts[:intensity]	
-    raise ArgumentError, "options[:intensity] is not a Numeric" if !@intensity.is_a?(Float)
-    raise RangeError, "options[:intensity] is outside the range 0.0..1.0" if !(0.0..1.0).include?(@intensity)
-	    
-    @seperation = opts[:seperation]
-    raise ArgumentError, "options[:seperation] is not a Numeric" if !@seperation.is_a?(Float)
-    raise RangeError, "options[:seperation] is outside the range 0.0..1.0" if !(0.0..1.0).include?(@seperation)
+  # Set the note duration.
+  # @param [Rational] duration The duration of the note.
+  # @raise [ArgumentError] if duration is not a Rational.
+  def duration= duration
+  	raise ArgumentError, "duration is not a Rational" if !duration.is_a?(Rational)    
+  	@duration = duration
+  end
+
+  # Set the note loudness.
+  # @param [Float] loudness The loudness of the note.
+  # @raise [ArgumentError] if loudness is not a Float.
+  # @raise [RangeError] if loudness is outside the range 0.0..1.0.
+  def loudness= loudness
+    raise ArgumentError, "loudness is not a Float" if !loudness.is_a?(Float)
+    raise RangeError, "loudness is outside the range 0.0..1.0" if !(0.0..1.0).include?(loudness)
+  	@loudness = loudness
+  end
+
+  # Set the note intensity.
+  # @param [Float] intensity The intensity of the note.
+  # @raise [ArgumentError] if intensity is not a Float.
+  # @raise [RangeError] if intensity is outside the range 0.0..1.0.
+  def intensity= intensity
+    raise ArgumentError, "intensity is not a Float" if !intensity.is_a?(Float)
+    raise RangeError, "intensity is outside the range 0.0..1.0" if !(0.0..1.0).include?(intensity)
+	@intensity = intensity
+  end
+
+  # Set the note seperation.
+  # @param [Float] seperation The seperation of the note.
+  # @raise [ArgumentError] if seperation is not a Float.
+  # @raise [RangeError] if seperation is outside the range 0.0..1.0.
+  def seperation= seperation
+    raise ArgumentError, "seperation is not a Float" if !seperation.is_a?(Float)
+    raise RangeError, "seperation is outside the range 0.0..1.0" if !(0.0..1.0).include?(seperation)
+    @seperation = seperation
   end
 end
 
--- spec/core/note_spec.rb
+++ spec/core/note_spec.rb
@@ -10,16 +10,71 @@ describe Musicality::Note do
     lambda { Musicality::Note.new @pitch, 1.to_r }.should_not raise_error RangeError
   end
   
-  it "should assign pitch and duration parameters" do
+  it "should assign pitch and duration parameters when given during construction" do
     note = Musicality::Note.new @pitch, 2.to_r
     note.pitch.ratio.should eq(@pitch.ratio)
     note.duration.should eq(Rational(2,1)) 
   end
 
-  it "should assign :loudness, :intensity, and :seperation parameters" do
+  it "should assign :loudness, :intensity, and :seperation parameters if given during construction" do
     note = Musicality::Note.new @pitch, 2.to_r, :loudness => 0.1, :intensity => 0.2, :seperation => 0.3
     note.loudness.should eq(0.1)
     note.intensity.should eq(0.2)
     note.seperation.should eq(0.3)
+  end
+
+  it "should assign :tie and :slur parameters if given during construction" do
+    note = Musicality::Note.new @pitch, 2.to_r, :tie => false, :slur => false
+    note.tie.should be_false
+    note.slur.should be_false
+
+    note = Musicality::Note.new @pitch, 2.to_r, :tie => true, :slur => true
+    note.tie.should be_true
+    note.slur.should be_true
+  end
+  
+  it "should assign pitch" do
+    note = Musicality::Note.new @pitch, 2.to_r
+    new_pitch = Musicality::Pitch.new :ratio => 55.0
+    note.pitch = new_pitch
+    note.pitch.ratio.should eq new_pitch.ratio
+  end
+
+  it "should assign duration" do
+    note = Musicality::Note.new @pitch, 2.to_r
+    note.duration = 3.to_r
+    note.duration.should eq 3.to_r
+  end
+
+  it "should assign loudness" do
+    note = Musicality::Note.new @pitch, 2.to_r
+    note.loudness = 0.123
+    note.loudness.should eq 0.123
+  end
+
+  it "should assign intensity" do
+    note = Musicality::Note.new @pitch, 2.to_r
+    note.intensity = 0.123
+    note.intensity.should eq 0.123
+  end
+  
+  it "should assign seperation" do
+    note = Musicality::Note.new @pitch, 2.to_r
+    note.seperation = 0.123
+    note.seperation.should eq 0.123
+  end
+
+  it "should assign tie" do
+    note = Musicality::Note.new @pitch, 2.to_r
+    note.tie.should be_false
+    note.tie = true
+    note.tie.should be_true
+  end
+  
+  it "should assign slur" do
+    note = Musicality::Note.new @pitch, 2.to_r
+    note.slur.should be_false
+    note.slur = true
+    note.slur.should be_true
   end
 end
