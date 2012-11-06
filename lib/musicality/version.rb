db7c3f9e4e41a61c9ba9a8306dbcda56d6a367fd
James Tunnell <jamestunnell@lavabit.com>
2012-11-06 11:53:37 -0800

    Increment revision number, making the current version 0.1.1.

--- lib/musicality/version.rb
+++ lib/musicality/version.rb
@@ -1,5 +1,5 @@
-# Representation musical constructs such as pitch, note, etc. 
+# Representation musical constructs such as pitch, note, score, etc. 
 module Musicality
   # musicality version
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
