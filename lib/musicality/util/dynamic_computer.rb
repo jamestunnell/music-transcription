9254cdee22be3939cd7b38413e684cb8f046401c
James Tunnell <jamestunnell@lavabit.com>
2012-11-21 11:24:43 -0800

    Change TempoComputer and DynamicComputer to use ValueComputer as base class.

--- lib/musicality/util/dynamic_computer.rb
+++ lib/musicality/util/dynamic_computer.rb
@@ -1,87 +1,33 @@
 module Musicality
 
-#
+# Computer dynamic at any offset. Uses ValueComputer base class to perform computation.
 #
 # @author James Tunnell
 #
-# @!attribute [r] piecewise_function
-#   @return [PiecewiseFunction] A piecewise function that can calculate the 
-#                               dynamic loudness for any valid note offset.
-#
-class DynamicComputer
-  attr_reader :piecewise_function
+class DynamicComputer < ValueComputer
   
   # A new instance of DynamicComputer.
-  # @param [Dynamic] start_dynamic The dynamic to use at start.
+  # @param [Tempo] start_dynamic The dynamic to use at start.
   # @param [Array] dynamic_changes An array of dynamic events.
-  # @raise [ArgumentError] if start_dynamic is not a Dynamic.
-  # @raise [ArgumentError] if any of dynamic_changes is not a Dynamic.
-  # @raise [ArgumentError] if the starting dynamic has a non-zero duration.
+  # @raise [ArgumentError] if start_dynamic is not a Tempo.
+  # @raise [ArgumentError] if any of dynamic_changes is not a Tempo.
   def initialize start_dynamic, dynamic_changes = []
-    raise ArgumentError, "start_dynamic is not a Dynamic" if !start_dynamic.is_a?(Dynamic)
-    raise ArgumentError, "starting dynamic cannot have a non-zero event duration" if start_dynamic.duration != 0
-    
-    @piecewise_function = Musicality::PiecewiseFunction.new
-    set_default_dynamic start_dynamic
-    
-    if dynamic_changes.any?
-      dynamic_changes = Event.hash_events_by_offset dynamic_changes
-      sorted_dynamic_change_offsets = dynamic_changes.keys.sort
-        
-      for i in 0...sorted_dynamic_change_offsets.count do
-        offset = sorted_dynamic_change_offsets[i]
-        dynamic = dynamic_changes[offset]
-        
-        if offset < start_dynamic.offset
-          add_to_piecewise_function dynamic, dynamic.offset...start_dynamic.offset
-        else
-          add_to_piecewise_function dynamic, dynamic.offset...(Event::MAX_OFFSET + 1)
-        end
-      end
-    end
-  end
-  
-  # Compute the dynamic loudness at the given note offset.
-  # @param [Rational] note_offset The given note offset to compute dynamic at.
-  def loudness_at note_offset
-    @piecewise_function.evaluate_at note_offset
-  end
-  
-  private
+    raise ArgumentError, "start_dynamic is not a Dynamic" unless start_dynamic.is_a?(Dynamic)
 
-  def set_default_dynamic dynamic
-    func = lambda {|x| dynamic.loudness }
-    @piecewise_function.add_piece (Event::MIN_OFFSET)...(Event::MAX_OFFSET + 1), func
-  end
-
-  # Add a function piece to the piecewise function, which will to compute dynamic
-  # for a matching note offset. If the dynamic event duration is non-zero, a 
-  # linear transition function is created.
-  #
-  # @param [Dynamic] dynamic The Dynamic object which contains offset, duration, and
-  #                      dynamic information.
-  def add_to_piecewise_function dynamic, domain
-    
-    func = nil
-    
-    if dynamic.duration == 0
-      func = lambda {|x| dynamic.loudness }
-    else
-      b = @piecewise_function.evaluate_at domain.first
-      m = (dynamic.loudness - b) / dynamic.duration
-      
-      func = lambda do |x|
-        raise RangeError, "#{x} is not in the domain" if !domain.include?(x)
-        
-        if x < (domain.first + dynamic.duration)
-          (m * (x - domain.first)) + b
-        else
-          dynamic.loudness
-        end
-      end
+    default_value = start_dynamic.loudness
+    value_change_events = []
+    dynamic_changes.each do |dynamic_change|
+      raise ArgumentError, "dynamic_change #{dynamic_change} is not a Dynamic" unless dynamic_change.is_a?(Dynamic)
+      value_change_events << Event.new(dynamic_change.offset, dynamic_change.loudness, dynamic_change.duration)
     end
     
-    @piecewise_function.add_piece domain, func
+    super(default_value, value_change_events)
+  end
+  
+  # Compute the dynamic loudness at the given offset.
+  # @param [Numeric] offset The given offset to compute dynamic at.
+  def loudness_at offset
+    value_at offset
   end
 end
 
--- lib/musicality/util/tempo_computer.rb
+++ lib/musicality/util/tempo_computer.rb
@@ -1,89 +1,33 @@
 module Musicality
 
-#
+# Computer tempo at any offset. Uses ValueComputer base class to perform computation.
 #
 # @author James Tunnell
 #
-# @!attribute [r] piecewise_function
-#   @return [PiecewiseFunction] A piecewise function that can calculate the 
-#                               tempo in notes per second for any valid note offset.
-#
-class TempoComputer
-  attr_reader :piecewise_function
+class TempoComputer < ValueComputer
   
   # A new instance of TempoComputer.
   # @param [Tempo] start_tempo The tempo to use at start.
   # @param [Array] tempo_changes An array of tempo events.
   # @raise [ArgumentError] if start_tempo is not a Tempo.
   # @raise [ArgumentError] if any of tempo_changes is not a Tempo.
-  # @raise [ArgumentError] if the starting tempo has a non-zero duration.
   def initialize start_tempo, tempo_changes = []
     raise ArgumentError, "start_tempo is not a Tempo" if !start_tempo.is_a?(Tempo)
-    raise ArgumentError, "starting tempo cannot have a non-zero event duration" if start_tempo.duration != 0
-    
-    @piecewise_function = Musicality::PiecewiseFunction.new
-    set_default_tempo start_tempo
-    
-    if tempo_changes.any?
-      tempo_changes = Event.hash_events_by_offset tempo_changes
-      sorted_tempo_change_offsets = tempo_changes.keys.sort
-        
-      for i in 0...sorted_tempo_change_offsets.count do
-        offset = sorted_tempo_change_offsets[i]
-        tempo = tempo_changes[offset]
-        
-        if offset < start_tempo.offset
-          add_to_piecewise_function tempo, tempo.offset...start_tempo.offset
-        else
-          add_to_piecewise_function tempo, tempo.offset...(Event::MAX_OFFSET + 1)
-        end
-      end
-    end
-  end
-  
-  # Compute the tempo in notes per second at the given note offset.
-  # @param [Rational] note_offset The given note offset to compute tempo at.
-  def notes_per_second_at note_offset
-    @piecewise_function.evaluate_at note_offset
-  end
-  
-  private
 
-  def set_default_tempo tempo
-    notes_per_sec = (tempo.beats_per_minute / 60.0) * tempo.beat_duration
-    func = lambda {|x| notes_per_sec }
-    @piecewise_function.add_piece( (Event::MIN_OFFSET)...(Event::MAX_OFFSET + 1), func )
-  end
-
-  # Add a function piece to the piecewise function, which will to compute tempo
-  # for a matching note offset. If the tempo event duration is non-zero, a 
-  # linear transition function is created.
-  #
-  # @param [Tempo] tempo The Tempo object which contains offset, duration, and
-  #                      tempo information.
-  def add_to_piecewise_function tempo, domain
-    
-    notes_per_sec = (tempo.beats_per_minute / 60.0) * tempo.beat_duration
-    func = nil
-    
-    if tempo.duration == 0
-      func = lambda {|x| notes_per_sec }
-    else
-      b = @piecewise_function.evaluate_at domain.first
-      m = (notes_per_sec - b) / tempo.duration
-      
-      func = lambda do |x|
-        raise RangeError, "#{x} is not in the domain" if !domain.include?(x)
-        
-        if x < (domain.first + tempo.duration)
-          (m * (x - domain.first)) + b
-        else
-          notes_per_sec
-        end
-      end
+    default_value = start_tempo.notes_per_second
+    value_change_events = []
+    tempo_changes.each do |tempo_change|
+      raise ArgumentError, "tempo_change #{tempo_change} is not a Tempo" unless tempo_change.is_a?(Tempo)
+      value_change_events << Event.new(tempo_change.offset, tempo_change.notes_per_second, tempo_change.duration)
     end
     
-    @piecewise_function.add_piece domain, func
+    super(default_value, value_change_events)
+  end
+  
+  # Compute the tempo in notes per second at the given offset.
+  # @param [Numeric] offset The given offset to compute tempo at.
+  def notes_per_second_at offset
+    value_at offset
   end
 end
 
