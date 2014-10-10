fd8f19df580088ecbc46b4558ebd153f22e88214
James Tunnell <jamestunnell@gmail.com>
2014-10-10 14:17:50 -0700

    Add parsing convenience methods for both single and whitespace sequences of durations, pitches, and notes

---
+++ lib/music-transcription/parsing/convenience_methods.rb
@@ -0,0 +1,83 @@
+module Music
+module Transcription
+module Parsing
+  DURATION_PARSER = DurationParser.new
+  PITCH_PARSER = PitchParser.new
+  NOTE_PARSER = NoteParser.new
+  
+  def duration dur_str
+    DURATION_PARSER.parse(dur_str).to_r
+  end
+  alias :dur :duration
+  module_function :duration
+  module_function :dur
+  
+  def durations durs_str
+    durs_str.split.map do |dur_str|
+      duration(dur_str)
+    end
+  end
+  alias :durs :durations
+  module_function :durs
+  module_function :durations
+  
+  def pitch p_str
+    PITCH_PARSER.parse(p_str).to_pitch
+  end
+  module_function :pitch
+
+  def pitches ps_str
+    ps_str.split.map do |p_str|
+      pitch(p_str)
+    end
+  end
+  module_function :pitches
+  
+  def note note_str
+    NOTE_PARSER.parse(note_str).to_note
+  end
+  module_function :note
+  
+  def notes notes_str
+    notes_str.split.map do |note_str|
+      note(note_str)
+    end
+  end
+  module_function :notes
+end
+end
+end
+
+class String
+  def to_duration
+    Music::Transcription::Parsing::duration(self)
+  end
+  alias :to_dur :to_duration
+  alias :to_d :to_duration
+  
+  def to_durations
+    Music::Transcription::Parsing::durations(self)
+  end
+  alias :to_durs :to_durations
+  alias :to_ds :to_durations
+  
+  def to_pitch
+    Music::Transcription::Parsing::pitch(self)
+  end
+  alias :to_p :to_pitch
+  
+  def to_pitches
+    Music::Transcription::Parsing::pitches(self)
+  end
+  alias :to_ps :to_pitches
+  
+  def to_note
+    Music::Transcription::Parsing::note(self)
+  end
+  alias :to_n :to_note
+  
+  def to_notes
+    Music::Transcription::Parsing::notes(self)
+  end
+  alias :to_ns :to_notes
+end
---
+++ spec/parsing/convenience_methods_spec.rb
@@ -0,0 +1,89 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+dur_stuff = ['should parse as single duration',
+{
+  '/2' => "1/2".to_r,
+  '5/29' => "5/29".to_r,
+  '200/' => "200/1".to_r,
+  '66' => "66/1".to_r
+}]
+
+durs_stuff = ['should parse as whitespace-separated durations',
+{
+  '/2 5/29 200/ 66' => ["1/2".to_r,"5/29".to_r,"200/1".to_r,"66/1".to_r],
+  "/2\t5/29\n200/   \t\n  66" => ["1/2".to_r,"5/29".to_r,"200/1".to_r,"66/1".to_r],
+}]
+
+pitch_stuff = ['should parse as single pitch',
+{
+  'C2' => C2,
+  'Db4' => Db4,
+  'A#9' => Bb9
+}]
+
+pitches_stuff = ['should parse as whitespace-separated pitches',
+{
+  'C2 C2 D2 C2' => [C2,C2,D2,C2],
+  "Bb2\tF5  \n  Gb7" => [Bb2,F5,Gb7],
+}]
+
+note_stuff = ['should parse as a single note',
+{
+  '/2' => Note::half,
+  '99/10C2' => Note.new('99/10'.to_r, [C2]),
+  '5/2.Db4,Eb5' => Note.new('5/2'.to_r, [Db4,Eb5], articulation:STACCATO)
+}]
+
+notes_stuff = ['should parse as whitespace-separated notes',
+{
+  '/2 /2 /4' => [Note::half,Note::half,Note::quarter],
+  "/4C4  \t  /4D4" => [Note::quarter([C4]),Note::quarter([D4])],
+  "/2Db2\t/2C2  \n /2C2" => [Note::half([Db2]), Note::half([C2]), Note::half([C2])]
+}]
+
+{
+  :duration => dur_stuff,
+  :dur => dur_stuff,
+  :durations => durs_stuff,
+  :durs => durs_stuff,
+  :pitch => pitch_stuff,
+  :pitches => pitches_stuff,
+  :note => note_stuff,
+  :notes => notes_stuff
+}.each do |mod_fun,descr_cases|
+  describe("Parsing::" + mod_fun.to_s) do 
+    descr, cases = descr_cases
+    it descr do
+      cases.each do |s,tgt|
+        Parsing.send(mod_fun,s).should eq tgt
+      end
+    end
+  end
+end
+
+{
+  :to_d => dur_stuff,
+  :to_dur => dur_stuff,
+  :to_duration => dur_stuff,
+  :to_ds => durs_stuff,
+  :to_durs => durs_stuff,
+  :to_durations => durs_stuff,
+  :to_p => pitch_stuff,
+  :to_pitch => pitch_stuff,
+  :to_ps=> pitches_stuff,
+  :to_pitches => pitches_stuff,
+  :to_n => note_stuff,
+  :to_note => note_stuff,
+  :to_ns => notes_stuff,
+  :to_notes => notes_stuff
+}.each do |inst_meth,descr_cases|
+  describe("String#" + inst_meth.to_s) do 
+    descr, cases = descr_cases
+    it descr do
+      cases.each do |s,tgt|
+        s.send(inst_meth).should eq tgt
+      end
+    end
+  end
+end
+
