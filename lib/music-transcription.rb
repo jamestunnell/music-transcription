78fde0a0ed98f3313d87dbd3c7153ab7f812160c
James Tunnell <jamestunnell@gmail.com>
2014-10-12 21:35:06 -0700

    Add pitch printing

--- lib/music-transcription.rb
+++ lib/music-transcription.rb
@@ -32,3 +32,5 @@ require 'music-transcription/parsing/note_parsing'
 require 'music-transcription/parsing/note_nodes'
 require 'music-transcription/parsing/meter_parsing'
 require 'music-transcription/parsing/convenience_methods'
+
+require 'music-transcription/printing/pitch_printing'
